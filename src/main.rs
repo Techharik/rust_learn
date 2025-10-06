1e566b26238edd6163fd2fed4a21559f74f18daf
Hari Prasath <khariprasath30@gmail.com>
2025-10-06 16:46:36 +0530

    fibnocci using rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("{}", "Hello world")
+    println!("{}", "My rust revision here");
+    // fibnocii seq algo in using rust;
+
+    let fibseq = fib(5);
+
+    println!("{}", fibseq)
+}
+
+fn fib(num: u64) -> u64 {
+    if num == 0 {
+        return 1;
+    }
+
+    return num * fib(num - 1);
 }
