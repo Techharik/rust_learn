954da4ff1a073e332081945549aa3692bdee83bd
Hari Prasath <khariprasath30@gmail.com>
2025-10-06 17:07:20 +0530

    added the fib and str function

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,45 @@ fn main() {
 
     let fibseq = fib(5);
 
-    println!("{}", fibseq)
+    let str = String::from("Hello h");
+    let length_str = str_len(&str);
+
+    println!("{}", str);
+    println!("{}", length_str);
+    println!("{}", fibseq);
+    println!("{}", fibser(3))
 }
 
+// fib - serious;
 fn fib(num: u64) -> u64 {
-    if num == 0 {
+    if num <= 1 {
         return 1;
     }
 
     return num * fib(num - 1);
+}
+
+fn fibser(num: u32) -> u32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
+    }
+
+    if num == 1 {
+        return second;
+    }
+
+    for _ in 0..(num - 1) {
+        println!("I am printing");
+        let temp = second;
+        second = second + first;
+        first = temp
+    }
+    return second;
+}
+
+fn str_len(str: &String) -> usize {
+    return str.chars().count();
 }
